01fe8a332616441d734984f533ec4ed2ac060dd5
Molaie <amirmahdimolaee88@gmail.com>
2025-07-18 17:03:25 +0200

    Creating ejs file and connect the css file to it. Ctaete background header.

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ const app = express();
 // Creating localhost
 app.listen(3000, ()=> console.log('Connect'));
 
+// Setting the ejs technology
+app.set('view engine', 'ejs');
+
+// Connectin the public folder 
+app.use(express.static('public'));
+
 // Connectin test
-app.get('/', (req, res) => res.send('Hello!'))
+app.get('/', (req, res) => {
+    res.render('./home.ejs')
+})
