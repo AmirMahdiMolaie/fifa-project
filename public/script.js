f731766a219c2f6b7fe70dacb8e757a2b80661d8
Molaie <amirmahdimolaee88@gmail.com>
2025-07-21 17:09:28 +0200

    Create an animation to change the positions of game slides.

--- public/script.js
+++ public/script.js
@@ -1,2 +1,39 @@
 let menuHeader = () => document.querySelector('body').classList.toggle('active_menu');
 let rankHander = () => document.querySelector('body').classList.toggle('active_rank');
+
+let listPlay = [
+    {
+        current : 'position3',
+        next : 'position2',
+    },
+    {
+        current : 'position2',
+        next : 'position1',
+    },
+    {
+        current : 'position1',
+        next : 'position5',
+    },
+    {
+        current : 'position5',
+        next : 'position3',
+    },
+    // {
+    //     current : 'position4',
+    //     next : 'position5',
+    // },
+]
+const handlerPlay = ()=> {
+    let play = document.querySelectorAll('.partion-play');
+    for(let img of play){
+        for(i in listPlay){
+            if(img.classList.contains(listPlay[i].current)){
+                img.classList.replace(listPlay[i].current,listPlay[i].next);
+                break;
+            }
+        }
+    }
+}
+setInterval(()=> {
+    handlerPlay()
+},3000);
