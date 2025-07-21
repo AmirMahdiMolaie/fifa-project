66f1955abe17a2e7af8735d7b14e08e974178b26
Molaie <amirmahdimolaee88@gmail.com>
2025-07-22 00:26:20 +0200

    Making changes to files and adding db files and codes for an introduction for the future.

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 // Require the framworks
 const express = require('express');
+const { default: mongoose } = require('mongoose');
+const morgan = require('morgan')
 
 // Creating vars
 const app = express();
-
-// Creating localhost
-app.listen(3000, ()=> console.log('Connect'));
 
 // Setting the ejs technology
 app.set('view engine', 'ejs');
@@ -14,9 +13,41 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static('partials'));
 
+// Conect to db for future
+const dbURI = 'mongodb+srv://amirmahdimolaie:amir1388@nodeproject.ci16kx4.mongodb.net/?retryWrites=true&w=majority&appName=Nodeproject';
+
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
+    const Blog = require('./blog')
+
+    app.get('/blog', (req, res) => {
+        const blog = new Blog({
+            name : 'amir',
+            age : 13,
+            number : 9012,
+        })
+
+    blog.save()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err))
+        })
+
+    app.get('/all-blog', (req, res) =>{
+        Blog.find()
+            .then((result) => res.send(result))
+            .catch((err) => console.log(err))
+    })
+    app.get('/d all-blog', (req, res) =>{
+        Blog.deleteMany()
+            .then((result) => res.send(result))
+            .catch((err) => console.log(err))
+    })
+
 // Connectin test
 app.get('/', (req, res) => {
-    res.render('./home.ejs', {
+    res.render('./home', {
         title : 'FIFA | Home',
         content1 : 'FIFA World Cup 26™ Ticketing Programme launches this September',
         content2 : 'President Trump says FIFA is doing “fantastically well” in growing football in the United States',
@@ -29,7 +60,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/tournaments', (req, res)=>{
-    res.render('./tournaments.ejs', {
+    res.render('./tournaments', {
         title : 'FIFA | tournaments',
         content1 : 'FIFA World Cup 26™',
         content2 : 'FIFA Women’s World Cup Brazil 2027™ ',
@@ -42,29 +73,28 @@ app.get('/tournaments', (req, res)=>{
 });
 
 app.get('/world-rankings', (req, res)=>{
-    res.render('./world-rankings.ejs', {
+    res.render('./world-rankings', {
         title : 'FIFA | world-rankings',
         classBody : 'body-ranking',
-    });
-});
-
-app.get('/watch-on-fifa', (req, res)=>{
-    res.render('./watch-on-fifa.ejs', {
-        title : 'FIFA | watch on fifa',
-        classBody : 'body-home'
+        picture : 'imeges/FIFAPLUS_BEST-ON-SHOW_Tournaments_00.webp',
+        content : 'Ranking table of the top 20 countries in the world'
     });
 });
 
 app.get('/play', (req, res)=>{
-    res.render('./play.ejs', {
+    res.render('./play', {
         title : 'FIFA | play',
-        classBody : 'body-homes'
+        classBody : 'body-ranking',
+        picture : 'imeges/FIFA-Futsal-World-Cup-Uzbekistan-2024-Final.avif',
+        content : 'Games program'
     });
 });
 
 app.use((req, res) =>{
-    res.render('./404.ejs', {
+    res.render('./404', {
         title : '404 | not found',
         classBody : 'body-home'
     });
 })
+
+console.log(app.use(morgan('tiny')));
